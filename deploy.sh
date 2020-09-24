4b8beefb396e827cb706399f8740f6b16397ec5c
Shekar <mcsr1977@gmail.com>
2020-09-25 01:57:52 +0530

    updated the deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,3 @@ docker push stephengrider/multi-worker:latest
 docker push stephengrider/multi-client:$SHA
 docker push stephengrider/multi-server:$SHA
 docker push stephengrider/multi-worker:$SHA
-
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
