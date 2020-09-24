68dcc30a61fc41504a1ec92a28587b19a8f85a96
Shekar <mcsr1977@gmail.com>
2020-09-25 03:10:01 +0530

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t mcsr1977/multi-client:latest -t mcsr1977/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mcsr1977/multi-server:latest -t mcsr1977/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mcsr1977/multi-worker:latest -t mcsr1977/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push mcsr1977/multi-client:latest
+docker push mcsr1977/multi-server:latest
+docker push mcsr1977/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push mcsr1977/multi-client:$SHA
+docker push mcsr1977/multi-server:$SHA
+docker push mcsr1977/multi-worker:$SHA
