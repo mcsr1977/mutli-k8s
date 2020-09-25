a68bf9b504926efd9ea94a01705168ab21887d2d
Shekar <mcsr1977@gmail.com>
2020-09-25 08:04:07 +0530

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,8 @@ docker push mcsr1977/multi-worker:latest
 docker push mcsr1977/multi-client:$SHA
 docker push mcsr1977/multi-server:$SHA
 docker push mcsr1977/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mcsr1977/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mcsr1977/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mcsr1977/multi-worker:$SHA
